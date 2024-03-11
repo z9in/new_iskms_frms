3e21b8a2184875091899c90decfbec67d38e6ac5
z9in <113665653+z9in@users.noreply.github.com>
2024-03-11 20:42:57 +0900

    Add files via upload

--- pages/data_check.js
+++ pages/data_check.js
@@ -17,14 +17,14 @@
     }
 
     // Use facingMode: environment to attemt to get the front camera on phones
-    // check_btn.addEventListener('click',()=>{
+    check_btn.addEventListener('click',()=>{
       navigator.mediaDevices.getUserMedia({ video: { facingMode: "environment" } }).then(function(stream) {
         video.srcObject = stream;
         video.setAttribute("playsinline", true); // required to tell iOS safari we don't want fullscreen
         video.play();
         requestAnimationFrame(tick);
       });
-    // })
+    })
     
 
     function tick() {
