5db8cb8ebba465f56ad4a4e79ce91e486b2f3920
z9in <113665653+z9in@users.noreply.github.com>
2024-03-12 07:01:03 +0900

    Add files via upload

--- pages/data_check.js
+++ pages/data_check.js
@@ -1,3 +1,38 @@
+let year_el = document.querySelectorAll('.year');
+let mon_el = document.querySelectorAll('.monthy');
+let day_el = document.querySelectorAll('.day');
+let hour_el = document.querySelectorAll('.hour');
+let minit_el = document.querySelectorAll('.minit');
+console.log(year_el)
+
+
+
+
+
+
+setInterval(()=>{
+    let dates = new Date()
+    year_el[0].innerHTML = dates.getFullYear()+"년 ";
+    // year_el[1].innerHTML = dates.getFullYear()+"년 ";
+    mon_el[0].innerHTML = dates.getMonth()+1+"월 ";
+    // mon_el[1].innerHTML = dates.getMonth()+1+"월 ";
+    day_el[0].innerHTML = dates.getDate()+"일 ";
+    // day_el[1].innerHTML = dates.getDate()+"일 ";
+    hour_el[0].innerHTML = dates.getHours()+"시 ";
+    if(dates.getMinutes()<10) {
+        minit_el[0].innerHTML = "0"+dates.getMinutes()+"분";
+    }else {
+        minit_el[0].innerHTML = dates.getMinutes()+"분";
+    }
+    // hour_el[1].innerHTML = dates.getHours()+"시 ";
+    // if(dates.getMinutes()<10) {
+    //     minit_el[1].innerHTML = "0"+dates.getMinutes()+"분";
+    // }else {
+    //     minit_el[1].innerHTML = dates.getMinutes()+"분";
+    // }
+},1000);
+
+
 var video = document.createElement("video");
 var canvasElement = document.getElementById("canvas");
 var canvas = canvasElement.getContext("2d");
@@ -18,8 +53,8 @@ function drawLine(begin, end, color) {
 check_btn.addEventListener('click', ()=>{
   navigator.mediaDevices.getUserMedia({ video: { 
     facingMode: "environment",
-    // width: {ideal: 300},
-    // height: {ideal:300}
+    width: {ideal: 300},
+    height: {ideal:300}
   } }).then(function(stream) {
     video.srcObject = stream;
     video.setAttribute("playsinline", true); // required to tell iOS safari we don't want fullscreen
