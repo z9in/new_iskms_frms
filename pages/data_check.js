686fa028e94847552d0778d592833b49d144b1d9
z9in <113665653+z9in@users.noreply.github.com>
2024-03-12 06:47:41 +0900

    Add files via upload

--- pages/data_check.js
+++ pages/data_check.js
@@ -18,8 +18,8 @@ function drawLine(begin, end, color) {
 // check_btn.addEventListener('click', ()=>{
   navigator.mediaDevices.getUserMedia({ video: { 
     facingMode: "environment",
-    width: {ideal: 300},
-    height: {ideal:300}
+    // width: {ideal: 300},
+    // height: {ideal:300}
   } }).then(function(stream) {
     video.srcObject = stream;
     video.setAttribute("playsinline", true); // required to tell iOS safari we don't want fullscreen
